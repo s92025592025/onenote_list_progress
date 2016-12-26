9061afe54dfb20de31d58094cdcd38d26547773f
Flying_Apple_Pie <s92025592025@gmail.com>
2016-12-26 14:31:30 -0800

    no

--- index.js
+++ index.js
@@ -70,14 +70,15 @@
 		function updateProgress(sectionPages, progressBar = todayProgress){
 			for(var i = 0; i < JSON.parse(sectionPages).value.length; i++){
 				if(Date.parse(JSON.parse(sectionPages).value[i].title) 
-					&& (new Date(JSON.parse(sectionPages).value[i].title)) - (new Date("2016/12/07")) 
-						<= (1000 * 60 * 60 * 24)){
+					&& Math.abs((new Date(JSON.parse(sectionPages).value[i].title)) - (new Date("2016/12/07")) 
+						<= (1000 * 60 * 60 * 24))){
+					console.log("once");
 					onenoteRequest('pages/' + JSON.parse(sectionPages).value[i].id, function (content) {
-						var parser = new DOMParser();
-						var dom = parser.parseFromString(content, 'text/html');
-						progressBar.animate(dom.querySelectorAll('p["data-tag"="to-do:complete"]').length 
-											/ (dom.querySelectorAll('p["data-tag"="to-do"]').length 
-												+ dom.querySelectorAll('p["data-tag"="to-do:complete"]').length));
+						var dom = document.createElement('html');
+						dom.innerHTML = content;
+						progressBar.animate(dom.querySelectorAll('p[data-tag="to-do:complete"]').length 
+											/ (dom.querySelectorAll('p[data-tag="to-do"]').length 
+												+ dom.querySelectorAll('p[data-tag="to-do:complete"]').length));
 					});
 				}
 			}
