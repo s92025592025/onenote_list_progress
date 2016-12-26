a51c97029978353222fa9eac0d32d94d1bcdc4d7
Flying_Apple_Pie <s92025592025@gmail.com>
2016-12-26 14:09:50 -0800

    implementing method to update today progress bar

--- index.js
+++ index.js
@@ -8,6 +8,7 @@
 	const {BrowserWindow} = require('electron').remote;
 	const fs = require('file-system');
 	const ProgressBar = require('progressbar.js');
+	const ONENOTE_ROOT = 'https://www.onenote.com/api/v1.0/me/notes/';
 
 	window.onload = function (){
 		if(!JSON.parse(fs.readFileSync('notebooks.json')).today_progress){
@@ -31,18 +32,97 @@
 		      settingWin = null;
 		    });
 		}else{
-			showToday();
-			showMisc();
+			showToday("a");
 		}
 	};
 
 	// pre: when user do have a section to store the checklist for today,
 	//		the page title should contain only data in any format
-	// post: show the progress of today in a circle progressbar
-	function showToday(){}
+	// post: create a new task bar
+	function showToday(){
+		var todayProgress = new ProgressBar.Circle('#today_progressbar', {
+			color: '#aaa',
+			trailColor: '#9D9E9E',
+			strokeWidth: 8,
+			trailWidth: 1,
+			easing: 'easeInOut',
+			duration: 1400,
+			text: {
+				autoStyleContainer: false
+			},
+			from: {color: '#94D0FF', width: 2},
+			to: {color: '#008FFF', width: 8},
+			step: function (state, circle){
+				circle.path.setAttribute('stroke', state.color);
+    			circle.path.setAttribute('stroke-width', state.width);
+    			circle.setText(Math.round(circle.value() * 1000) / 10 + "%");
+			}
+		});
+
+		todayProgress.text.style.fontFamily = 'Helvetica, sans-serif';
+		todayProgress.text.style.fontSize = '60pt';
+		todayProgress.animate(0.0);
+
+		function updateProgress(sectionPages, progressBar = todayProgress){
+			for(var i = 0; i < JSON.parse(sectionPages).value.length; i++){
+				if(Date.parse(JSON.parse(sectionPages).value[i].title) 
+					&& (new Date(JSON.parse(sectionPages).value[i].title)) - (new Date()) 
+						< (1000 * 60 * 60 * 24)){
+					
+				}
+			}
+		}
+	}
 
 	// pre: the sections the user specified to be tracked, title needs to
 	// 		have any date format in []
 	// post: show a list of progress in a linear progress bar
-	function showMisc(){}
+	function showMisc(json){}
+
+	// pre: path should only vaild request path specifed in onenote api,
+	//		should also pass in a funciton as nextStep that will be executed
+	//		onload, the function should all take only a json as parameter
+	// post: execute specifed process
+	function onenoteRequest(path, nextStep){
+		var request = new XMLHttpRequest();
+		request.open("GET", ONENOTE_ROOT + path, true);
+		request.setRequestHeader("Authorization", 
+				"Bearer " + JSON.parse(fs.readFileSync("token.json")).access_token);
+
+		request.onload = function (){
+			if(this.status == 200 || this.status == 0){
+				nextStep(this.responseText);
+			}else if(this.status = 401){
+				getNewAccess(path, nextStep);
+			}else{
+				console.log(this.status);
+				console.log(JSON.parse(this.responseText));
+			}
+		}
+
+		request.send();
+	}
+
+	// pre: when the access token has expired. Should pass in the path to later
+	//		re-request and the function to nextStep for post request action
+	// post: will get a new token and re-execute previous onenoteRequest
+	function getNewAccess(path, nextStep){
+		var request = new XMLHttpRequest();
+		request.open("POST", "https://login.live.com/oauth20_token.srf", true);
+		request.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+		request.onload = function (){
+			if(this.status == 200 || this.status == 0){
+				fs.writeFileSync('token.json', this.responseText);
+				onenoteRequest(path, nextStep);
+			}else{
+				console.log(this.status);
+				console.log(JSON.parse(this.responseText));
+			}
+		};
+
+		request.send("grant_type=refresh_token"
+					+ "&client_id=" + JSON.parse(fs.readFileSync("oauth2Info.json")).client_id
+					+ "&redirect_uri=" + JSON.parse(fs.readFileSync("oauth2Info.json")).redirect_uri
+					+ "&refresh_token=" + JSON.parse(fs.readFileSync("token.json")).refresh_token);
+	}
 })();
